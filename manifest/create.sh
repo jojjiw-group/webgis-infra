2441da32762b5fea8e8cec23e47d8bb43fee2ec6
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-09-14 15:02:32 +0000

    Setup PV/PVC for postgis

--- manifest/create.sh
+++ manifest/create.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 set -x
-#kubectl create -R -f storage
-#sleep 2
+kubectl create -R -f storage
+sleep 2
 kubectl create -R -f deployments
 sleep 2
 kubectl create -R -f services
